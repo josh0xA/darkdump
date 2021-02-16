e43aefbbdd500935af90a3d69251c4cee642f5fb
Josh Schiavone <jshschiavone@gmail.com>
2021-02-15 19:01:43 -0500

    copyright year fix

--- darkdump.py
+++ darkdump.py
@@ -1,7 +1,7 @@
 '''
 MIT License
 
-Copyright (c) 2020 Josh Schiavone
+Copyright (c) 2021 Josh Schiavone
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
